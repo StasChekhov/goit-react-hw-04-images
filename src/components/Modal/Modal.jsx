b89ca18f3d52960ab2236b0a36ba8d89a4990dc8
Стас Чехов <staschekhov@MacBook-Pro-Stas.local>
2022-02-16 19:36:56 +0200

    hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,48 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css'
 import PropTypes from "prop-types";
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
+export default function Modal ({image, onClose}) {
 
-    componentDidMount() {
-        window.addEventListener('keydown', this.escapeFunction)
-    }
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.escapeFunction)
-    }
-    
-    escapeFunction = e => {
-        if (e.code === 'Escape') {
-            this.props.onClose()
+    useEffect(() => {
+        const escapeFunction = e => {
+            if (e.code === 'Escape') {
+                onClose()
+            }
+        };
+            window.addEventListener('keydown', escapeFunction);
+        return () => {
+            window.removeEventListener('keydown', escapeFunction)
         }
-    }
+    },[ onClose ])
+    // componentDidMount() {
+    //     window.addEventListener('keydown', this.escapeFunction)
+    // }
+    // componentWillUnmount() {
+    //     window.removeEventListener('keydown', this.escapeFunction)
+    // }
+    
 
-    overlayClick = e => {
+    const overlayClick = e => {
         if (e.currentTarget === e.target) {
-            this.props.onClose()
+            onClose()
         }
     }
     
-    render() { 
-        return createPortal(
-            <div className={s.overlay} onClick={this.overlayClick}>
+        return createPortal (
+            <div className={s.overlay} onClick={overlayClick}>
                 <div className={s.module}>
-                    <img  src={this.props.image} alt="" />
+                    <img  src={image} alt="" />
                 </div>
             </div>,
             modalRoot,
         );
-    }
 }
  
-export default Modal;
+
 
 Modal.propTypes = {
   image: PropTypes.string,
